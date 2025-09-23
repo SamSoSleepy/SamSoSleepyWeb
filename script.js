82709ae546103effb5cd249d2645bd8f37dca4af
SamSoSleepy <127093457+SamSoSleepy@users.noreply.github.com>
2025-09-23 19:26:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("magic-btn").addEventListener("click", () => {
+  alert("✨ You feel the power of the Dark World!");
+  document.body.style.backgroundColor = "#000033"; // เปลี่ยนสีพื้นหลัง
+});
