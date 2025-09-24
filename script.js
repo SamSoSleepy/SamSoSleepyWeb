5e659bebef9044a3118f5d0193ff978e1ce110a5
SamSoSleepy <127093457+SamSoSleepy@users.noreply.github.com>
2025-09-24 17:32:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const freeAddonBtn = document.getElementById('freeAddonBtn');
     const boxContainer = document.getElementById('box-container');
+    const mainContent = document.getElementById('main-content');
 
     freeAddonBtn.addEventListener('click', (e) => {
-        e.preventDefault(); // ป้องกันการเปลี่ยนหน้าเมื่อคลิก
+        e.preventDefault();
 
         if (boxContainer.classList.contains('hidden')) {
             boxContainer.classList.remove('hidden');
+            mainContent.classList.add('hidden');
         } else {
             boxContainer.classList.add('hidden');
+            mainContent.classList.remove('hidden');
         }
     });
 });
