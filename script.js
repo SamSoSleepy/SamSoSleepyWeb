a32c7974808a6d51cf5f86ad5b9ebcac560a3164
SamSoSleepy <127093457+SamSoSleepy@users.noreply.github.com>
2025-09-24 20:50:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Array of music playlists - easy to add more songs
     const musicPlaylist = [
         {
-            id: '7f1RK1m7qvc',
+            id: '0p5ZiillFdA',
             title: 'Current Song'
         }
         // เพิ่มเพลงใหม่ได้ที่นี่:
